29b298a6ae74b02c6a75e0939a8b8ac6e9ebbe35
Francois Gervais <francoisgervais@gmail.com>
2019-04-19 14:24:56 -0400

    Get commands from the serial port

--- gateway.py
+++ gateway.py
@@ -1,9 +1,23 @@
 import keyboard
+import serial
 import time
 
 while True:
-    keyboard.write(' ')
+    with serial.Serial('/dev/ttyACM0', 115200) as ser:
+        token = ser.read()
+        # print(token)
+
+    if token == b'l':
+        # print("keyboard write")
+        # keyboard.write(' ')
+        keyboard.press_and_release('left')
+
+    elif token == b'r':
+        keyboard.press_and_release('right')
+    elif token == b'u':
+        # keyboard.press_and_release('up')
+        keyboard.write(' ')
 
     # keyboard.wait('esc')
 
-    time.sleep(1)
+    # time.sleep(1)
